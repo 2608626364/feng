ccb0cdfc14a01bd098cce0a173c52ee5e3013b5d
2608626364@qq.com <991029.f>
2020-08-15 00:55:51 +0800

    添加前端页面

--- src/main/java/com/fengfan/myspringboot/server/UserService.java
+++ src/main/java/com/fengfan/myspringboot/server/UserService.java
@@ -1,6 +1,6 @@
 package com.fengfan.myspringboot.server;
 
-import com.fengfan.myspringboot.mapper.UserDetailMapper;
+import com.fengfan.myspringboot.dao.UserDetailMapper;
 import com.fengfan.myspringboot.pojo.UserDetail;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,6 +8,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
+
 @Service
 public class UserService  implements UserDetailsService {
     @Resource
@@ -16,6 +18,8 @@ public class UserService  implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserDetail user = new UserDetail();
         user.setUserName(username);
+        List<UserDetail> details = userDetailMapper.selectAll();
+
         UserDetail userDetail = userDetailMapper.selectByUserDetail(user);
         if (user == null) {
             throw new UsernameNotFoundException("用户不存在！");
