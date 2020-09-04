b493d9767ce3783bd90840d73666bfaafeafa84b
fengfan <2608626364@qq.com>
2020-09-04 20:35:22 +0800

    修改一下下

--- src/main/java/com/fengfan/utils/upload/upload.java
+++ src/main/java/com/fengfan/utils/upload/upload.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller("/upload")
 public class upload {
     @RequestMapping("/Img")
-    public String uploadImg(@RequestParam("file") MultipartFile file, @RequestParam("filegather") MultipartFile[] filegather) {
+    public String uploadImg(@RequestParam("file") MultipartFile file, @RequestParam("filegather") MultipartFile[] filegather, HttpServletRequest request) {
         System.out.println("单个文件");
         System.out.println(file);
         for (int i = 0; i < filegather.length; i++) {
